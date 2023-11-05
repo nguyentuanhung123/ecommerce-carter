bc92ad431fb4172f930386a758fd204429866dd3
nguyentuanhung123 <nguyentuanhung4529871036@gmail.com>
2023-11-05 16:42:46 +0700

    add components , router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+//import react router
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+//import pages
+import Home from "./pages/Home.jsx";
+import ProductDetails from "./pages/ProductDetails.jsx"
+
+//import components
+import Sidebar from "./components/Sidebar.jsx";
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
 
 function App() {
   return (
-    <div className="App">
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <div className='overflow-hidden'>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
