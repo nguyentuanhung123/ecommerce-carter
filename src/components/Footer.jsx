d1856025fe6a9222b75ac081ca4e6dc44eb5d92c
nguyentuanhung123 <nguyentuanhung4529871036@gmail.com>
2023-11-12 23:39:38 +0700

    edit p on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = () => {
     <div className=' bg-[#333] py-16 px-4 grid lg:grid-cols-3 gap-8 text-gray-300'>
       <div>
         <h1 className='w-full text-3xl font-bold text-[#00df9a]'>REACT.</h1>
-        <p className='py-4'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id odit ullam iste repellat consequatur libero reiciendis, blanditiis accusantium.</p>
+        <p className='py-4'>The way to sell clothes effectively is to differentiate your store as well as the clothing products you are selling. It's very simple, you just need to have a little bit of your own aesthetic, combine them together into a separate style instead of selling separate products.​</p>
         <div className='flex justify-between md:w-[75%] my-6'>
           <FaFacebookSquare size={30} />
           <FaInstagram size={30} />
