9b71f7cd1947d214056273e6e4ad39e0d64af07b
nguyentuanhung123 <nguyentuanhung4529871036@gmail.com>
2023-12-13 22:11:16 +0700

    changes 10:10

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Loading = () => {
     return (
-        <div>Loading...on loading/Hung at 13/12/2023</div>
+        <div>Loading...on loading/Hung at 13/12/2023 10:10</div>
     )
 }
 
